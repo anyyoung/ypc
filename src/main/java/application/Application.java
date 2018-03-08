3b9763fa4d1a14026bf90f35d69e60fda055b505
angyang <angyang2016@163.com>
2018-03-08 14:15:08 +0800

    结构调整

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -1,11 +1,13 @@
-package server;
+package application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication 
-public class RpcBootstrap {
+
+@SpringBootApplication
+public class Application {
     
     public static void main(String[] args) {
-        SpringApplication.run(RpcBootstrap.class ,args);
+        SpringApplication.run(Application.class, args);
     }
+    
 }
